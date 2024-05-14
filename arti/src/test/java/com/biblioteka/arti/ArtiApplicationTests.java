240d275594b1d607b604fd2bc2d88c150ee34ba1
Marko Pesic <markopesic1605@sveti-sava.edu.rs>
2024-05-14 18:12:33 +0200

    Initial commit

---
+++ arti/src/test/java/com/biblioteka/arti/ArtiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteka.arti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
